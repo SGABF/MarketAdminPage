6ef42ba1870a29a29dd2059c7991a62f95f7fcb2
HanKyul <HanKyul@220.78.34.84>
2022-02-22 22:11:56 +0900

    First Test

---
+++ src/main/java/kr/green/core/Ex01SpringHelloWorld1Application.java
@@ -0,0 +1,13 @@
+package kr.green.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01SpringHelloWorld1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01SpringHelloWorld1Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/green/core/Ex01SpringHelloWorld1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.green.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01SpringHelloWorld1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
