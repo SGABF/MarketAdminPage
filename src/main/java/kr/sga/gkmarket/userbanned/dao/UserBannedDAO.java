327fd2bd44a90d73b0aebe6dd2cc506bfd844a5e
Hankyul <sosehe123@gmail.com>
2022-03-16 10:53:07 +0900

    리베이스를 위한 임시 커밋

--- src/main/java/kr/sga/gkmarket/userbanned/dao/UserBannedDAO.java
+++ src/main/java/kr/sga/gkmarket/userbanned/dao/UserBannedDAO.java
@@ -1,6 +1,13 @@
 package kr.sga.gkmarket.userbanned.dao;
 
+import java.util.List;
+
+import kr.sga.gkmarket.userbanned.vo.UserBannedVO;
+
 public interface UserBannedDAO {
+	
+	UserBannedVO getUser(int user_Idx);
+	
 	
 
 }
