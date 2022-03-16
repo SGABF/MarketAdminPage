535010060fe52e126a0de5554195184c2e9e5e26
Hankyul <sosehe123@gmail.com>
2022-03-16 11:02:42 +0900

    리베이스를 위한 임시커밋2

--- src/main/java/kr/sga/gkmarket/userbanned/dao/UserBannedDAO.java
+++ src/main/java/kr/sga/gkmarket/userbanned/dao/UserBannedDAO.java
@@ -1,13 +1,18 @@
 package kr.sga.gkmarket.userbanned.dao;
 
+import java.util.HashMap;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 
 import kr.sga.gkmarket.userbanned.vo.UserBannedVO;
 
+@Mapper
 public interface UserBannedDAO {
-	
+	// 관리할 유저 1개 불러오기
 	UserBannedVO getUser(int user_Idx);
-	
-	
-
+	// 관리할 유저 리스트 
+	List<UserBannedVO> getUserList();
+	// 유저 밴 활성화/비활성화
+	void userBannedActivate(HashMap<String, Integer> map);
 }
