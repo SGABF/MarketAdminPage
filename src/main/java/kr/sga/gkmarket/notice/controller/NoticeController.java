584913562cfd293beb291f7af79c183d64a95b52
Hankyul <sosehe123@gmail.com>
2022-03-21 17:58:45 +0900

    220321 공지사항 이미지 업로드 기능 구현

--- src/main/java/kr/sga/gkmarket/notice/controller/NoticeController.java
+++ src/main/java/kr/sga/gkmarket/notice/controller/NoticeController.java
@@ -38,8 +38,11 @@ public class NoticeController {
 	
     @Autowired
     private NoticeService noticeService; //서비스 연결
-    
+    @Autowired
+    private NoticeFileService noticeFileService;
 
+    private String os = System.getProperty("os.name").toLowerCase();
+    
     @GetMapping(value = "/notice") //주소 지정
     @ResponseBody
     public List<BackNoticeVO> getNoticeList(){
@@ -69,6 +72,60 @@ public class NoticeController {
         return "redirect:/notice";
     }
   
+//----------------------파일 업로드------------------------//
     
+    @GetMapping(value = "/noticefile")
+    @ResponseBody
+    public List<BackNoticeFileVO> getNoticeFileList(){
+    	
+    	List<BackNoticeFileVO> list = noticeFileService.getNoticeFileList();
+    	
+    	return list;
+    }
+   
+    @SuppressWarnings("deprecation")
+    @PostMapping(value = "/noticefile/insertFile")
+    @ResponseBody
+    public String insertFile(@RequestPart(value = "fileUp") MultipartFile mfile) {
+    	BackNoticeFileVO imageFile = new BackNoticeFileVO();
+    	String realPath = "";
+    	
+    	if(mfile != null) {
+			try {
+				if (os.contains("win")) {
+					realPath = "D:/image/";
+				} else {
+					realPath = "/resources/Back/";
+				}
+				String saveName = UUID.randomUUID() + "_" + mfile.getOriginalFilename();
+				
+				if(realPath != null && realPath != "") {
+					File target = new File(realPath, saveName);
+					mfile.transferTo(target);
+					
+					imageFile.setBack_Noticefile_OriName(mfile.getOriginalFilename());
+					imageFile.setBack_Noticefile_SaveName(saveName);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		noticeFileService.insertFile(imageFile);
+    	
+    	return imageFile.toString();
+    }
+    
+    @DeleteMapping(value = "noticefile/deleteFile")
+    @ResponseBody
+    public void deleteFile(@RequestParam int noticeFileId) {
+    	String realPath = "";
+    	if (os.contains("win")) {
+			realPath = "D:/image/";
+		} else {
+			realPath = "/resources/Back/";
+		}
+		
+    	noticeFileService.deleteFile(noticeFileId, realPath);
+    }
     
 }
