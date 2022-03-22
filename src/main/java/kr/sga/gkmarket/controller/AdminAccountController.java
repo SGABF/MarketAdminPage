c333d515efc43e66b3dfe7b11150dbd86d7c6e7b
CASY82 <repaid11010@gmail.com>
2022-03-22 10:56:51 +0900

    2022/03/22 Banner 기능쪽 Name규칙 변경

--- src/main/java/kr/sga/gkmarket/controller/AdminAccountController.java
+++ src/main/java/kr/sga/gkmarket/controller/AdminAccountController.java
@@ -1,8 +1,14 @@
 package kr.sga.gkmarket.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminAccountController {
+	
+	@PostMapping("/adminAC/addAdmin")
+	public void addAdminPost() {
+		
+	}
 	
 }
