be3284889930de89f30f4870380e1eb9e5a124e6
CASY82 <repaid11010@gmail.com>
2022-03-22 16:17:43 +0900

    2022/03/22 AdminAccount 기능 완성 및 Token DB연결 완료

--- src/main/java/kr/sga/gkmarket/security/dao/AdminDAO.java
+++ src/main/java/kr/sga/gkmarket/security/dao/AdminDAO.java
@@ -1,5 +1,8 @@
 package kr.sga.gkmarket.security.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.sga.gkmarket.security.vo.AdminVO;
@@ -7,4 +10,22 @@ import kr.sga.gkmarket.security.vo.AdminVO;
 @Mapper
 public interface AdminDAO {
 	AdminVO getUser();
+	
+	// 1. 1개의 계정 정보 얻기
+	AdminVO getAdminAccount(Map<String, String> map);
+	
+	// 2. 전체 계정 정보 얻기
+	List<AdminVO> getAllAdminAccount();
+	
+	// 3. 계정 추가
+	void addAdminAccount(AdminVO adminVO);
+	
+	// 4. 계정 삭제
+	void deleteAdminAccount(int admin_idx);
+	
+	// 5. 비밀번호 수정
+	void changePassword(AdminVO adminVO);
+	
+	// 6. 비밀번호 얻기
+	String getAdminPassword(String admin_id);
 }
--- src/main/java/kr/sga/gkmarket/security/service/AdminService.java
+++ src/main/java/kr/sga/gkmarket/security/service/AdminService.java
@@ -1,7 +1,28 @@
 package kr.sga.gkmarket.security.service;
 
+import java.util.List;
+import java.util.Map;
+
 import kr.sga.gkmarket.security.vo.AdminVO;
 
 public interface AdminService {
 	AdminVO getUser();
+	
+	// 1. 1개의 계정 정보 얻기
+	AdminVO getAdminAccount(String admin_id);
+	
+	// 2. 전체 계정 정보 얻기
+	List<AdminVO> getAllAdminAccount();
+	
+	// 3. 계정 추가
+	void addAdminAccount(AdminVO adminVO);
+	
+	// 4. 계정 삭제
+	void deleteAdminAccount(int admin_idx);
+	
+	// 5. 비밀번호 수정
+	void changePassword(AdminVO adminVO);
+	
+	// 6. 비밀번호 얻기
+	String getAdminPassword(String admin_id);
 }
